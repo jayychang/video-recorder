8c7806a626e543bf6c2ffe1999282db215c0b86a
Jay Chang <jaychanggg@gmail.com>
2016-12-13 12:56:25 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,80 @@
+var cameraStarted = false;
+var recording = false;
+var recorder;
+var chunks;
+
+function cameraToggle() {
+    if (!cameraStarted) {
+        startCamera();
+        return;
+    }
+
+    var v = document.getElementById('videoStream');
+    var button = document.getElementById('cameraButton');
+
+    if (v.paused || v.ended) {
+        button.innerHTML = "Pause Stream";
+        v.play();
+    } else {
+        button.innerHTML = "Resume Stream";
+        v.pause();
+    }
+}
+
+function recordVideo() {
+  var button = document.getElementById("recordButton")
+  if(recording) {
+    button.innerHTML = "Start Recording"
+    recorder.stop();
+    createVideo();
+    recording = !recording;
+  } else {
+    button.innerHTML = "Stop Recording"
+    chunks = [];
+    recorder.start();
+    recorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+    }
+    recording = !recording;
+  }
+}
+
+function createVideo() {
+  let blob = new Blob(chunks, {type: 'video/webm' })
+  let url = URL.createObjectURL(blob)
+  var recordedVideo = document.getElementById("recordedVideo");
+  recordedVideo.controls = true;
+  recordedVideo.src = url;
+}
+// camera setup
+function startCamera() {
+    // check for getUserMedia support
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia ||
+        navigator.oGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        // get webcam feed if available
+        navigator.getUserMedia({video: true, audio: true}, handleVideo, videoError);
+    }
+}
+
+function handleVideo(stream) {
+    // if found attach feed to video element
+    var video = document.querySelector("#videoStream");
+    if (video != null) {
+        video.src = window.URL.createObjectURL(stream);
+        recorder = new MediaRecorder(stream);
+    } else {
+        videoError("The camera is currently in use by another application or tab.")
+    }
+
+    cameraStarted = true;
+    cameraToggle();
+}
+
+function videoError(e) {
+    window.alert("Something went wrong:  \n\n" + e);
+}
